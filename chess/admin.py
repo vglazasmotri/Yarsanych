9acb8e490d05b499f697b2ed0bc0ee94a3f78fb5
SERGEYSYCH <vglazasmotri@yandex.ru>
2020-11-30 01:51:55 +0300

    change admin panel from Articles

--- chess/admin.py
+++ chess/admin.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import *
 
+
 class ReviewInline(admin.TabularInline):
     """Отзывы на статью"""
     model = Reviews
     extra = 1
     readonly_fields = ("name", "email")
 
-class ArticlesShotsInline(admin.TabularInline):
-    model = ArticlesShots
+
+class ArticleShotsInline(admin.TabularInline):
+    model = ArticleShots
     extra = 1
     readonly_fields = ("get_image",)
 
@@ -18,18 +20,35 @@ class ArticlesShotsInline(admin.TabularInline):
 
     get_image.short_description = "Изображение"
 
+
+@admin.register(Articles)
 class ArticlesAdmin(admin.ModelAdmin):
     """Статьи"""
     list_display = ("title", "date_pub", "slug")
     search_fields = ("title", "intro_text", "full_text")
-    inlines = [ArticlesShotsInline, ReviewInline]
+    inlines = [ArticleShotsInline, ReviewInline]
+    readonly_fields = ("get_image",)
+    fieldsets = (
+        (None, {
+            "fields": (("title", "slug",),)
+        }),
+        (None, {
+            "fields": ("intro_text", ("img", "get_image"))
+        }),
+        (None, {
+            "fields": (("full_text",),)
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.img.url} width="100" height="110"')
+
+    get_image.short_description = "Обложка статьи"
 
 
-admin.site.register(Articles, ArticlesAdmin)
 admin.site.register(Treners)
 admin.site.register(Files)
 admin.site.register(Videos)
 admin.site.register(Reviews)
 admin.site.register(Index)
 admin.site.register(Trenings)
-admin.site.register(ArticlesShots)
